bc855ff813c3b4ea9778be9c9281bf0b3b9a2c6f
wilsonyu111 <76260417+wilsonyu111@users.noreply.github.com>
2022-09-03 17:18:59 -0700

    new database and forntend
    adding mongodb as the new database
    creating a new frontend with graph display
    
    new database and forntend
    
    adding mongodb as the new database
    creating a new frontend with graph display
    
    new database and forntend
    
    adding mongodb as the new database
    creating a new frontend with graph display

--- sensor_server.py
+++ sensor_server.py
@@ -1,138 +1,73 @@
+from sensor_stat import sensor
+from logging import exception
 from flask import Flask, render_template, request, jsonify
-from datetime import datetime, timedelta
 import json
-import threading, time
+import datetime
+import json
 
-tempRecords ={}
-highestNum = 0
-timeUp = False # keep track of if the time is up
-COUNTDOWN = 10 # every 12 hours do a sensor check to see if any is offline
-#app = Flask(__name__, static_folder = "/home/pi/esp_sensor_server/templates")
+tempRecords = {}
+port = 8086
+
 app = Flask(__name__)
 
-'''
-# this is a temporary function used to test the servo
-# take this offline or make it avaliable only in private network
-# this function will send the servo testing html file
-# that allows for the control of a servo
-@app.route ("/fancontrol", methods=["GET"])
-def fanController():
 
-        return render_template ("fanControl.html", default_value = 20) # render fan control page
-
-# This is an overloaded fanController function
-# It takes a fanValue variable and update the html page with this value
-# the default value is 20
-def fanController(fanValue):
-        return render_template ("fanControl.html", default_value=fanValue) # render fan control page
-
-# This function is for updating/storing online fan controlling devices
-# If a device has been inactive for 24 hour, it should be removed from
-# the array (decide what data structure to use)
-# 
-@app.route ("/fanControllerOnline", methods=["PUT"])
-def updateControllerProfile():
-
-        # store the ip of each controller
-        # store the time when it's online
-        # store the location name
-        # store mac or chip id??
-        # if it already exist update these values
-        return None
-
-
-@app.route ("/fancontrol", methods=["POST"])
-def fanControllerValue():
-        sliderValue = request.form["fanSpeed"] # get the fan speed value from form request
-        print (sliderValue) 
-        return fanController(sliderValue) # return the original page, will change later
-'''
-@app.route('/', methods=['GET']) # default home page
+@app.route('/', methods=['GET'])  # default home page
 def sensors():
 
-        return render_template ("main.html", temps= tempRecords, size= highestNum +1) # render html file in templates folder
-        #return "hello!"
+    # render html file in templates folder
+    return render_template("home.html")
 
-@app.route ('/sendtoserver', methods= ['POST']) #receive data from the sensors
-def get_data ():
+# return a dictionary containing all sensor object's data
+@app.route('/getData', methods=['GET'])
+def getData():
+
+        debug = {}
+        for i in tempRecords:
+                dictionary = tempRecords[i].getDict()
+                debug[i] = dictionary
+                
+        return json.dumps(debug, indent=4)
+
+
+@app.route('/sendtoserver', methods=['POST'])  # receive data from the sensors
+def receiveData():
 
         data = request.get_json()
-        tempF = data["temp"]
-        hud = data["hud"]
-        location = data["location"] # remember to update the controllers to send only numbers!!!!
-        if "light" in data:
+        tempF = int(data["temp"])
+        hud = int(data["hud"])
+        sensor_id = data["sensor_id"]
 
-                light_status = data["light"]
-        else:
-                light_status = "N/A"
+        if (sensor_id in tempRecords):  # if the key exist
 
-        noSensor = ["no sensor","","","",""]
-        global timeUp
+            updateSesnor = tempRecords[sensor_id]  # get the sensor object
+            # updates temp, hud and last active
+            updateSesnor.updateData(hud, tempF)
+            print(updateSesnor.toString())
 
-        # to reference a global variable, you need to tell python this is a global variable
-        # by doing the following and then you will be able to use this
-        # I think for variables that are pass by reference you are not required to do this
-        global highestNum 
+        else:  # if a location does not exist in dictionary add it
 
-        #how to read the dictionary of array
-        #dictionary by location
-        # each dictionary contain an array
-        # [0] = room location
-        # [1] = temp in F
-        # [2] = hudmidity
-        # [3] = last active
-        # [4] = light is on or off
-        #print ("length of records: " + str(len(tempRecords)))
+            # create a new sensor object for holding sensor informations
+            tempRecords[sensor_id] = sensor(
+                sensor_id=sensor_id, temp=tempF, hudm=hud)
 
-        if int(location) > highestNum:
-                #since the html need to loop through the dictionary using index
-                #and all sensors are named based on numbers
-                #This will give me the highest number that are connected
-                #so I can loop through all these sensors up till the highest one
-                
-                highestNum = int(location)
+        return "received", 200
 
-        if (location in tempRecords): # if the key exist
+def getTimeStamp():
+    # returns the current time in military time (UTC) format
+    return datetime.datetime.now().replace(microsecond=0).isoformat()+"Z"
 
-                tempRecords[location][1] = tempF # update temp in F
-                tempRecords[location][2] = hud # hudmidity
-                tempRecords[location][3] = datetime.now().strftime("%Y/%m/%d %I:%M %p") # update time in 12 hour format
-                tempRecords[location][4] = light_status
-                
-        else: # if a location does not exist in dictionary add it
-                tempArr = ["room " + location, tempF, hud, datetime.now().strftime("%Y/%m/%d %I:%M %p"), light_status] # update time in 12 hour format
-                tempRecords[location] = tempArr # add an array to the dictionary pair
-        	
-        return "received"
 
-def countDown (): # this function does a countdown to however many seconds and set timeUp to True
-        
-        global COUNTDOWN
-        global timeUp
-        time.sleep(COUNTDOWN)
-        timeUp = True
+def getMonth():
+    # Return the current month as a string
+    return datetime.datetime.now().strftime("%B")
 
-def startTimer():
-        timer = threading.Thread(target=countDown)
-        timer.start()
 
-def lastActive(previousTime):
-        diff = str(datetime.now() - previousTime)
-        if "days," in diff: #if there exist days in the difference return how many days since last active
-                day = diff.split("day")
-                return day[0] + " day(s)"
-        else: # if there only exist hours, minutes or seconds
-                time = diff.split(":")
+def getYear():
+    # Return the current year as a string
+    return datetime.datetime.now().strftime("%Y")
 
-                if time[0] != "0": #return hours since last active
-                        return time[0] + " hour(s) ago"
-                elif time[1] != "00":
-                        return time[1] + " minute(s) ago"# return minutes since last active
-                else:
-                        return time[2][0:2] + " second(s) ago"# return seconds since last active
-        
-if __name__ == "__main__": # in order to run it as main, it needs to be ran as python "file_name"
 
-        startTimer() # to excute functions, you need to put it before app.run
-	app.run(debug=True, host= '0.0.0.0', port= "5000") #allow the server to run in debug mode without setting it in console
+if __name__ == "__main__":  # in order to run it as main, it needs to be ran as python "file_name"
 
+    # allow the server to run in debug mode without setting it in console
+    app.run(debug=True, host='0.0.0.0', port="5000")
---
+++ sensor_stat.py
@@ -0,0 +1,68 @@
+import datetime
+# import pymongo
+# from mongoengine import *
+
+class sensor:
+
+    def __init__(self, sensor_id:str, temp:int,hudm:int, database="N/A"):
+        self.temp = temp
+        self.hudm = hudm
+        self.lastActive = self.__getLastActive()
+        self.sensor_id = sensor_id
+        self.database = database
+        self.highest = temp
+        self.lowest = temp
+        self.tempAvg = temp
+        self.hudmAvg = hudm
+        self.__dataCount = 1
+        self.__tempSum = temp
+        self.__hudmSum = hudm
+
+    def __updateHighAndLowTemp(self):
+
+        if self.highest < self.temp:
+            self.highest = self.temp
+
+        if self.lowest > self.temp:
+            self.lowest = self.temp
+    
+    def __updateAvg(self):
+        self.__hudmSum += self.hudm
+        self.__tempSum += self.temp
+        self.__dataCount += 1
+        self.tempAvg = self.__tempSum / self.__dataCount
+        self.hudmAvg = self.__hudmSum / self.__dataCount
+        
+
+    def __getLastActive(self):
+        return datetime.datetime.now().strftime(
+            "%Y/%m/%d %I:%M %p")
+
+    def updateData(self, hud:int, temp:int):
+        self.temp = temp
+        self.hudm = hud
+        self.lastActive = self.__getLastActive()
+        self.__updateAvg()
+        self.__updateHighAndLowTemp()
+
+    def toString(self):
+        return "temperature: " + str(self.temp) \
+            + "\nhudmidity: " + str(self.hudm) \
+            + "\nlast active: " + self.lastActive \
+            + "\nlocation: " + str(self.sensor_id) \
+            + "\nhigh: " + str(self.highest) \
+            + "\nlow: " + str(self.lowest) \
+            + "\ntemperature average: " + str(self.tempAvg) \
+            + "\nhudmidity average: " + str(self.hudmAvg)
+
+    def getDict(self):
+        return {
+            "temperature": self.temp,
+            "hudmidity": self.hudm,
+            "last active": self.lastActive,
+            "location": self.sensor_id,
+            "highest" : self.highest,
+            "lowest" : self.lowest,
+            "temperature average" : self.tempAvg,
+            "hudmidity average" : self.hudmAvg
+        }
