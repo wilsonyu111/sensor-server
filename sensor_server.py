cfcd5f4902dfdf38a5943f90ad25aab5868d3f9f
wilsonyu111 <wilsonyu009@gmail.com>
2022-10-10 22:59:17 -0700

    adding communication between sensors and server reading sensor config reading sensor value telling sensor to restart via http updating sensor config using http

--- sensor_server.py
+++ sensor_server.py
@@ -1,24 +1,33 @@
 from sensor_stat import sensor
-# from flask_cors import CORS 
-from flask import Flask, request, send_from_directory, session
+from flask_cors import CORS 
+from flask import Flask, request, send_from_directory, session, make_response
 import json
 import datetime
 import bcrypt
+import secrets
+import base64
+from http import cookies
+import requests
 
 tempRecords = {}
 port = 8086
-database = {}
+test_Uname = "wilson"
+test_Pwd="password"
+database = {test_Uname:bcrypt.hashpw(test_Pwd.encode(), bcrypt.gensalt())}
+SESSION_ID_LENGTH = 50
 
-# app = Flask(__name__)
 app = Flask(__name__, static_url_path='', static_folder='build')
-# CORS(app)
+CORS(app)
 app.secret_key = "testsession"
 
+# ----------------------------------------------------------------------------------
 
 @app.route("/",  methods=['GET'])
 def sensors():
     return send_from_directory(app.static_folder,'index.html')
 
+
+# ----------------------------------------------------------------------------------
 # return a dictionary containing all sensor object's data
 @app.route('/getData', methods=['GET'])
 def getData():
@@ -30,28 +39,89 @@ def getData():
                 
         return json.dumps(debug, indent=4)
 
+# ----------------------------------------------------------------------------------
 
 @app.route('/sendtoserver', methods=['POST'])  # receive data from the sensors
 def receiveData():
 
         data = request.get_json()
-        tempF = float(data["temp"])
-        hud = float(data["hud"])
-        sensor_id = data["location"]
+        tempF = float(data["temperature"])
+        hud = float(data["humidity"])
+        sensor_id = data["MAC_ADDRESS"]
+        location_name = data["location_name"]
+        lightStat=data["light_status"]
+        ipadd=data["sensor_IP"]
+        sensor_port=data["sensor_listen_port"]
 
         if (sensor_id in tempRecords):  # if the key exist
 
             updateSesnor = tempRecords[sensor_id]  # get the sensor object
             # updates temp, hud and last active
-            updateSesnor.updateData(hud, tempF)
+            updateSesnor.updateData(hud=hud, temp=tempF, lightStat=lightStat, ipadd=ipadd, listen_port=sensor_port)
 
         else:  # if a location does not exist in dictionary add it
 
             # create a new sensor object for holding sensor informations
             tempRecords[sensor_id] = sensor(
-                sensor_id=sensor_id, temp=tempF, hudm=hud)
+                sensor_id=sensor_id, temp=tempF, hud=hud, location_name=location_name, lightStat=lightStat, ipadd=ipadd, listen_port=sensor_port)
 
         return "received", 200
+    
+
+
+# ----------------------------------------------------------------------------------
+    
+@app.route('/restartSensor', methods=['GET'])
+def restartSensor():
+    
+    (message, status_code) = sendHttpToSensor(req=request, URL_path="/restart")
+    return message, status_code
+
+# ----------------------------------------------------------------------------------
+
+@app.route('/sensorOnline', methods=['GET'])
+def sensorHeartBeat():
+    
+    (message, status_code) = sendHttpToSensor(req=request, URL_path="/online")
+    return message, status_code
+
+# ----------------------------------------------------------------------------------
+@app.route('/sensorConfig', methods=['GET'])
+def sensorConfig():
+    
+    (message, status_code, jsonBody) = sendHttpToSensor(req=request, URL_path="/serverStat")
+    return json.dumps(jsonBody,  indent=1), status_code
+
+# ----------------------------------------------------------------------------------
+    
+def sendHttpToSensor(req, URL_path):
+    macadd = ""    
+    try:
+        data = req.get_json()
+        macadd = data["macadd"]
+    except:
+        return ("bad request", 400)
+    
+    if macadd == "" or macadd not in tempRecords:
+        return ("Not Found", 404)
+    
+    
+    sensorInfo = tempRecords[macadd]
+    httpURI = makeURI(sensorInfo.ipadd, sensorInfo.listen_port, URL_path)
+    
+    response = requests.get(url = httpURI)
+    
+    try:
+        return (response.text, response.status_code, response.json())
+    except:
+        return (response.text, response.status_code)
+
+# ----------------------------------------------------------------------------------
+
+def makeURI(ip:str, port:int, path:str):
+    return "http://" + ip +":" + str(port) + path          
+
+# ----------------------------------------------------------------------------------
 
 @app.route('/login', methods=['POST'])  # login
 def login():
@@ -64,15 +134,21 @@ def login():
         return "bad request", 400
     
     if username in database and bcrypt.checkpw(password, database[username]):
-        session[username] = username
-        return "logged in", 200
+        
+        sessionID = generateSessionID(SESSION_ID_LENGTH, username)
+        print(sessionID)
+        # sessionCookie = "sessionId="+sessionID+"; Max-Age="+str(300)+";"
+        resp = make_response()
+        resp.set_cookie('sessionID', value=sessionID, max_age=300)
+        return resp, 200
     elif username in database:
         return "wrong password", 401
     elif username not in database:
-        return "user does not exist", 400
+        return "user does not exist", 401
     else:
         return "server error", 500
     
+# ----------------------------------------------------------------------------------
 
 @app.route('/logout', methods=['POST'])  # login
 def logout():
@@ -92,7 +168,8 @@ def logout():
         return "bad request", 400
     else:
         return "server error", 500
-        
+
+# ----------------------------------------------------------------------------------        
 
 @app.route('/signup', methods=['POST'])
 def signup():
@@ -109,6 +186,8 @@ def signup():
         database[username] = password
         return "user created", 201 
 
+# ----------------------------------------------------------------------------------
+
 @app.route('/showdatabase', methods=['GET'])
 def debugDatabase():
     
@@ -118,6 +197,24 @@ def debugDatabase():
         
     
     return json.dumps(debugData, indent=4), 200
+
+# ----------------------------------------------------------------------------------
+
+@app.route('/testCookie', methods=['POST'])
+def testCookie():
+    resp = make_response()
+    resp.set_cookie('username', 'the username')
+    return resp
+
+# ----------------------------------------------------------------------------------
+
+def generateSessionID(bstrSize, username):
+    data = secrets.token_bytes(nbytes=bstrSize)
+    sessionID = base64.b64encode(data).decode('utf-8')
+    session[sessionID] = username
+    return sessionID
+    
+    
     
 
 def getTimeStamp():
@@ -139,3 +236,4 @@ if __name__ == "__main__":  # in order to run it as main, it needs to be ran as 
 
     # allow the server to run in debug mode without setting it in console
     app.run(debug=True, host='0.0.0.0', port="5000")
+    
--- sensor_stat.py
+++ sensor_stat.py
@@ -1,22 +1,34 @@
 import datetime
+from http import server
+from time import sleep
 
 class sensor:
 
-    def __init__(self, sensor_id:str, temp:float,hudm:float, database="N/A"):
+    def __init__(self, sensor_id:str, location_name:str, temp:float,hud:float, lightStat:str, ipadd:str, listen_port:int):
         self.temp = temp
-        self.hudm = hudm
+        self.hud = hud
         self.lastActive = self.__getLastActive()
         self.sensor_id = sensor_id
-        self.database = database
+        self.location_name = location_name
         self.highest = temp
         self.lowest = temp
         self.tempAvg = temp
-        self.hudmAvg = hudm
+        self.hudAvg = hud
+        self.lightStatus = lightStat
         self.__dataCount = 1
         self.__tempSum = temp
-        self.__hudmSum = hudm
+        self.__hudSum = hud
+        self.ipadd = ipadd
+        self.listen_port = listen_port
+        self.configTime = -1
+        self.ssid = ""
+        self.installed_light = False
+        self.destPort = -1
+        self.sleepTimmr = -1
+        
 
     def __updateHighAndLowTemp(self):
+
 
         if self.highest < self.temp:
             self.highest = self.temp
@@ -24,43 +36,65 @@ class sensor:
         if self.lowest > self.temp:
             self.lowest = self.temp
     
+    def updateDeviceConfig(self, configTime:int, ssid:str, installed_light:bool, destPort:int, sleepTimer: int):
+        self.configTime=configTime
+        self.ssid = ssid
+        self.installed_light = installed_light
+        self.destPort = destPort
+        self.sleepTimmr = sleepTimer
+    
+    def compareTime(self, serverTime:int, sensorTime:int):
+        return serverTime == sensorTime
+
+    
     def __updateAvg(self):
-        self.__hudmSum += self.hudm
+        self.__hudSum += self.hud
         self.__tempSum += self.temp
         self.__dataCount += 1
         self.tempAvg = self.__tempSum / self.__dataCount
-        self.hudmAvg = self.__hudmSum / self.__dataCount
+        self.hudAvg = self.__hudSum / self.__dataCount
         
 
     def __getLastActive(self):
         return datetime.datetime.now().strftime(
             "%Y/%m/%d %I:%M %p")
 
-    def updateData(self, hud:int, temp:int):
+    def updateData(self, hud:int, temp:int, lightStat:str, ipadd:str, listen_port:int):
         self.temp = temp
-        self.hudm = hud
+        self.hud = hud
+        self.lightStatus = lightStat
+        self.ipadd = ipadd
+        self.listen_port = listen_port
         self.lastActive = self.__getLastActive()
         self.__updateAvg()
         self.__updateHighAndLowTemp()
 
     def toString(self):
         return "temperature: " + str(self.temp) \
-            + "\nhudmidity: " + str(self.hudm) \
+            + "\nhumidity: " + str(self.hud) \
+            + "\nlight status: " + self.lightStatus \
             + "\nlast active: " + self.lastActive \
-            + "\nlocation: " + str(self.sensor_id) \
+            + "\nlocation: " + self.location_name \
+            + "\nmac address: " + self.sensor_id \
+            + "\nip address: " + self.ipadd\
+            + "\nlistening port: " + self.listen_port\
             + "\nhigh: " + str(self.highest) \
             + "\nlow: " + str(self.lowest) \
             + "\ntemperature average: " + str(self.tempAvg) \
-            + "\nhudmidity average: " + str(self.hudmAvg)
+            + "\nhumidity average: " + str(self.hudAvg)
 
     def getDict(self):
         return {
             "temperature": self.temp,
-            "hudmidity": self.hudm,
-            "last active": self.lastActive,
-            "location": self.sensor_id,
+            "humidity": self.hud,
+            "light_status":self.lightStatus,
+            "last_active": self.lastActive,
+            "location": self.location_name,
+            "mac_address": self.sensor_id,
+            "ip_address": self.ipadd,
+            "port": self.listen_port,
             "highest" : self.highest,
             "lowest" : self.lowest,
             "temperature average" : self.tempAvg,
-            "hudmidity average" : self.hudmAvg
+            "humidity average" : self.hudAvg
         }
